247577377d7de40dda89cd129276725e93c43d6f
Linden Reid <thelinreid@gmail.com>
2018-09-21 23:24:32 -0500

    added some temp art assets
    huge thank you to the unity asset store dot corn

---
+++ Ragamuffin/Assets/Universal Pixel Hero (Warrior)/Scripts/Controller.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	Animator animator;
+	public float speed = 1f;
+
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	void Update () {
+		animator.speed = speed;
+	}
+
+	public void run() {
+		animator.SetTrigger ("run");
+	}
+
+	public void jump() {
+		animator.SetTrigger ("jump");
+	}
+
+	public void attack1() {
+		animator.SetTrigger ("attack1");
+	}
+
+	public void attack2() {
+		animator.SetTrigger ("attack2");
+	}
+
+	public void attack3() {
+		animator.SetTrigger ("attack3");
+	}
+
+	public void skill() {
+		animator.SetTrigger ("skill");
+	}
+}
